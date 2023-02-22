dcdde4ecd2f002f3a99e56addddc260445ce1a4d
unknown <ehdro0650@naver.com>
2023-02-22 17:49:43 +0900

    asdasd

--- CMNAI_3/common/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-
-class User(AbstractUser):
-    family = models.CharField(max_length=150, blank=True)
-    number = models.IntegerField()
-    phone = models.IntegerField()
